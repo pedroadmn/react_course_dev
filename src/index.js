f21acf756d1174f2117078f2ad2eff91a4e7c163
Pedro Adelino <pedro.mello@virtus.ufcg.edu.br>
2019-11-14 13:19:37 -0300

    First Commit - Starting the course

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import PrimeiroComponente from './components/PrimeiroComponente'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const element = document.getElementById('root');
+ReactDOM.render(<div><PrimeiroComponente /></div>, element); //JSX permite usar tags html no contexto de código javascript
