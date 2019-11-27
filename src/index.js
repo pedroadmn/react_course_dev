23c6dea4a24c31d722881e615254dfe0f13c51f9
Pedro Adelino <pedro.mello@virtus.ufcg.edu.br>
2019-11-27 07:37:38 -0300

    Fundaments about Component Relationship (Part 1)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import PrimeiroComponente from './components/PrimeiroComponente';
-import CompA, { CompB as B } from './components/DoisComponentes';
+// import CompA, { CompB as B } from './components/DoisComponentes';
 // import MultiElementos from './components/MultiElementos';
+import FamiliaSilva from './components/FamiliaSilva';
 
 const element = document.getElementById('root');
 ReactDOM.render(
     <div>
+        <FamiliaSilva/>
         {/*<MultiElementos />*/}
-        <CompA valor="Olá eu sou A!" />
-        <B valor="Olá eu sou B!" />
+        {/*<CompA valor="Olá eu sou A!" />
+        <B valor="Olá eu sou B!" />*/}
         {/*<PrimeiroComponente valor="Bom dia" aBcD={123} />*/}
     </div>,
     element
