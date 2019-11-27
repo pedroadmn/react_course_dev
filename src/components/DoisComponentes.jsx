bb5fa8de7ea743efc3ef604e0d0f047e2973f84c
Pedro Adelino <pedro.mello@virtus.ufcg.edu.br>
2019-11-27 07:25:01 -0300

    Fundaments about Module system (Import/Export)

--- src/components/DoisComponentes.jsx
+++ src/components/DoisComponentes.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const CompA = props =>
+export const CompA = props =>
     <h1>Primeiro diz: {props.valor}</h1>
 
-const CompB = props =>
+export const CompB = props =>
     <h1>Segundo diz: {props.valor}</h1>
 
+        // export default function(){
+        //     return <p>teste</p>
+        // } // Não posso retornar essa funcão sem usar o default ja que estou usando o export para cada component.
+
 export { CompA, CompB }
+export default CompA;
