24f9826e5f0eae510b5db1f7cad7779684af7d5e
Pedro Adelino <pedro.mello@virtus.ufcg.edu.br>
2019-11-28 07:11:22 -0300

    Fundaments about sending props to children component(1 member)

--- src/components/Familia.jsx
+++ src/components/Familia.jsx
@@ -3,5 +3,12 @@ import React from 'react';
 export default props =>
         <div>
             <h1>Família {props.sobrenome}</h1>
-            {props.children}
+            {React.cloneElement(props.children, { ...props })}
+
+            {/*{React.cloneElement(props.children, props)}  Menos usado*/}
+
+            {/*<h1>Família {props.sobrenome}</h1>
+            {React.cloneElement(props.children, { sobrenome: props.sobrenome })}*/}
+
+            {/*{props.children}*/}
         </div>
