fda2248a56884fc8f4ded85658d9dc5c7f7f0699
Pedro Adelino <pedro.mello@virtus.ufcg.edu.br>
2020-02-11 13:28:20 -0300

    Introduction to Hooks

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import ReactDOM from 'react-dom';
 // import ComponenteComFuncao from './components/ComponenteComFuncao';
 // import Pai from './components/Pai';
 // import ComponenteClasse from './components/ComponenteClasse';
-import Contador from './components/Contador';
+// import Contador from './components/Contador';
+import Hook from './components/Hook';
 
 const element = document.getElementById('root');
 ReactDOM.render(
     <div>
-        <Contador numeroInicial={100}/>
+        <Hook />
+        {/*<Contador numeroInicial={100}/>*/}
         {/*<ComponenteClasse valor="Sou um componente de classe"/>*/}
         {/*<Pai />*/}
         {/*<ComponenteComFuncao></ComponenteComFuncao>*/}
